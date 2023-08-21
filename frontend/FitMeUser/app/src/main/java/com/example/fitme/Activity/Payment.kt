791409987006ec667c8f282496cf282595cbcb2c
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-08-22 00:17:32 +0530

    addedPaymentConfirmAct

---
+++ frontend/FitMeUser/app/src/main/java/com/example/fitme/Activity/Payment.kt
@@ -0,0 +1,12 @@
+package com.example.fitme.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitme.R
+
+class Payment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+}
