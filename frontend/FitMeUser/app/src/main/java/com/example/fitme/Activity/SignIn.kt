24354c1ff14addeab050aeb3a6be8c6ea69676c5
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-08-21 10:14:49 +0530

    addedIcons

---
+++ frontend/FitMeUser/app/src/main/java/com/example/fitme/Activity/SignIn.kt
@@ -0,0 +1,12 @@
+package com.example.fitme.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitme.R
+
+class SignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
