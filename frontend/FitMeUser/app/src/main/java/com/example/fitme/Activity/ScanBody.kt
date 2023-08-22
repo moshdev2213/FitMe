b417ea190e3b31ca2cd95646da96e707d03ad186
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-08-22 20:40:47 +0530

    addedUiPicForScanBody

---
+++ frontend/FitMeUser/app/src/main/java/com/example/fitme/Activity/ScanBody.kt
@@ -0,0 +1,12 @@
+package com.example.fitme.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitme.R
+
+class ScanBody : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_body)
+    }
+}
