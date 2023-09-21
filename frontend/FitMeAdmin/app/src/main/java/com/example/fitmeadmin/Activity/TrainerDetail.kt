6d662c5c05781182ed13b2e307bdbf636ff7241c
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-09-21 17:52:31 +0530

    addedTrainerEditAdmin

---
+++ frontend/FitMeAdmin/app/src/main/java/com/example/fitmeadmin/Activity/TrainerDetail.kt
@@ -0,0 +1,12 @@
+package com.example.fitmeadmin.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitmeadmin.R
+
+class TrainerDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trainer_detail)
+    }
+}
