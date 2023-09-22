748061212b0f4bfde4cbbe83dd84c342fc6c49d1
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-09-22 17:48:56 +0530

    addedEditprofilesAdmin

---
+++ frontend/FitMeAdmin/app/src/main/java/com/example/fitmeadmin/Activity/MealEdit.kt
@@ -0,0 +1,12 @@
+package com.example.fitmeadmin.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitmeadmin.R
+
+class MealEdit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meal_edit)
+    }
+}
