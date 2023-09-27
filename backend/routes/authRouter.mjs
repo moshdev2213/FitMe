122c379603bbcbff283fa5a97de9f06d2da73a61
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-09-27 18:09:41 +0530

    signingOffNode

--- backend/routes/authRouter.mjs
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-import { authUser } from "../controller/authController.mjs";
-
-//get user auth
-router.post("/user", authUser);
-
-export { router as authRoutes};
